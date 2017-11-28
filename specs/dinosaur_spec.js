3f459dc1b899c759f018a48cbea0246eb58e81be
Findsay <fiona.e.lindsay@gmail.com>
2017-11-28 19:34:01 +0000

    Dinosaur spec and dinosaur class created and tests passing

---
+++ specs/dinosaur_spec.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+var Dinosaur = require('../dinosaur.js');
+
+var dinosaur;
+
+describe("Dinosaur", function(){
+  beforeEach(function(){
+    dinosaur = new Dinosaur("TRex", 1);
+  });
+
+  it("should have a type", function(){
+    assert.strictEqual(dinosaur.type, "TRex");
+  });
+
+  it("should have a number of offspring a year", function(){
+    assert.strictEqual(dinosaur.offspring, 1);
+  });
+
+
+
+
+
+});
